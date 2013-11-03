0d2534286f5d8a2b461460f000c0559c3e9ce0a5
Carolina A. Soler-Rodriguez <casoler11@gmail.com>
2013-11-03 00:52:07 -0500

    brakeman and figaro

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ end
 gem 'jquery-rails'
 gem "spork-rails"
 
+gem 'figaro'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -5,5 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 
-SampleApp::Application.config.secret_token = 'b65e7db91701b0b5c55586f2a712a3ee924740105121c4a8f75e84ffca4c01af723f803aca19ac65e308d4580c6ee8d43a975c725e29fc890d3e3f93f702b1f0'
-
+SampleApp::Application.config.secret_token = ENV['SECRET_TOKEN']
